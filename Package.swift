7c639d0e270ef4a91c3ad28362e96ed346544bb5
Joe Heck <heckj@mac.com>
2024-07-22 15:59:48 -0700

    first commit

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+var globalSwiftSettings: [PackageDescription.SwiftSetting] = [
+    .enableExperimentalFeature("StrictConcurrency")
+    // Swift 6 enablement
+    //.enableUpcomingFeature("StrictConcurrency")
+    //.swiftLanguageVersion(.v5)
+]
+
+let package = Package(
+    name: "Voxels",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+    ],
+    products: [
+        .library(
+            name: "Voxels",
+            targets: ["Voxels"]
+        ),
+        .executable(name: "voxel-benchmarks", targets: ["voxel-benchmarks"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.1"),
+    ],
+    targets: [
+        .target(
+            name: "Voxels",
+            dependencies: [],
+            swiftSettings: globalSwiftSettings),
+        .testTarget(
+            name: "VoxelsTests",
+            dependencies: ["Voxels"]
+        ),
+        .executableTarget(
+            name: "voxel-benchmarks",
+            dependencies: [
+                "Voxels",
+                .product(name: "CollectionsBenchmark", package: "swift-collections-benchmark"),
+            ]
+        ),    ]
+)
---
+++ Tests/VoxelsTests/VoxelsTests.swift
@@ -0,0 +1,12 @@
+@testable import Voxels
+import XCTest
+
+final class voxelsTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
