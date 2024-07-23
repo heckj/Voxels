35d1660beaa5bb82270d78d09a70257730e598ff
Joseph Heck <heckj@mac.com>
2024-07-22 17:33:04 -0700

    adding Vertex and extensions to support it (#2)
    * adding Vertex and extensions to support it
    * fixing non-simd compile
    * code cleanup

---
+++ Sources/Voxels/Vertex/CompleteVertex+generateMeshDescriptor.swift
@@ -0,0 +1,31 @@
+//  Created by Max Cobb on 12/06/2021.
+//  Used under MIT license, acquired from
+//  https://github.com/maxxfrazer/RealityGeometries/blob/main/Sources/RealityGeometries/CompleteVertex.swift
+//
+#if canImport(RealityKit)
+    import RealityKit
+
+    extension [CompleteVertex] {
+        func generateMeshDescriptor(
+            with indices: [UInt32], materials: [UInt32] = []
+        ) -> MeshDescriptor {
+            var meshDescriptor = MeshDescriptor()
+            var positions: [SIMD3<Float>] = []
+            var normals: [SIMD3<Float>] = []
+            var uvs: [SIMD2<Float>] = []
+            for vx in self {
+                positions.append(vx.position)
+                normals.append(vx.normal)
+                uvs.append(vx.uv)
+            }
+            meshDescriptor.positions = MeshBuffers.Positions(positions)
+            meshDescriptor.normals = MeshBuffers.Normals(normals)
+            meshDescriptor.textureCoordinates = MeshBuffers.TextureCoordinates(uvs)
+            meshDescriptor.primitives = .triangles(indices)
+            if !materials.isEmpty {
+                meshDescriptor.materials = MeshDescriptor.Materials.perFace(materials)
+            }
+            return meshDescriptor
+        }
+    }
+#endif
---
+++ Sources/Voxels/Vertex/CompleteVertex.swift
@@ -0,0 +1,49 @@
+public struct CompleteVertex: Hashable {
+    var position: SIMD3<Float>
+    var normal: SIMD3<Float>
+    var uv: SIMD2<Float>
+
+    public init(position: SIMD3<Float>, normal: SIMD3<Float>? = nil, uv: SIMD2<Float>? = nil) {
+        self.position = position
+        if let normal {
+            self.normal = normal.normalized()
+        } else {
+            self.normal = .zero
+        }
+        if let uv {
+            self.uv = uv
+        } else {
+            self.uv = .zero
+        }
+    }
+
+    /// Creates a new vertex with normal vector you provide.
+    /// - Parameter normal: The normal to apply to the vertex.
+    public func withNormal(_ normal: Vector) -> CompleteVertex {
+        CompleteVertex(position: position, normal: normal, uv: uv)
+    }
+
+    /// Creates a new vertex with the value for the normal inverted.
+    ///
+    /// Call to flip the orientation of the face of the polygon.
+    public func invertedNormal() -> CompleteVertex {
+        CompleteVertex(position: position, normal: -normal, uv: uv)
+    }
+
+    /// Linearly interpolate between two vertices.
+    ///
+    /// Interpolation is applied to the position, texture coordinate and normal.
+    public func lerp(_ other: CompleteVertex, _ t: Float) -> CompleteVertex {
+        CompleteVertex(
+            position: position.lerp(other.position, t),
+            normal: normal.lerp(other.normal, t),
+            uv: uv.lerp(other.uv, t)
+        )
+    }
+}
+
+public extension SIMD2<Float> {
+    func lerp(_ a: SIMD2<Float>, _ t: Float) -> SIMD2<Float> {
+        self + (a - self) * t
+    }
+}
