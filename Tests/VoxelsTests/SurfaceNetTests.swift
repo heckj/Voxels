898450f9ff7f1daa5c7b90ad7f45bfd77996ae88
Joe Heck <heckj@mac.com>
2024-07-24 13:09:43 -0700

    format fix

--- Tests/VoxelsTests/SurfaceNetTests.swift
+++ Tests/VoxelsTests/SurfaceNetTests.swift
@@ -32,7 +32,6 @@ final class SurfaceNetTests: XCTestCase {
         )
     }
 
-    
     func into_domain(array_dim: UInt, _ xyz: SIMD3<UInt>) -> SIMD3<Float> {
         // samples over a quadrant - starts at -1 and goes up to (2/edgeSize * (edgeSize-1)) - 1
         (2.0 / Float(array_dim)) * SIMD3<Float>(Float(xyz.x), Float(xyz.y), Float(xyz.z)) - 1.0
