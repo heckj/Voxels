c1d973a8c62dfa3a957690855945ac66b26c1f6a
Joe Heck <heckj@mac.com>
2024-07-26 15:26:40 -0700

    refining protocols for example usage

--- Sources/Voxels/SDF.swift
+++ Sources/Voxels/SDF.swift
@@ -1,22 +1,24 @@
 public protocol SignedDistanceField {
-    var isNegative: Bool { get }
+    associatedtype VoxelDataType where VoxelDataType: SIMDScalar
+    func valueAt(x: VoxelDataType, y: VoxelDataType, z: VoxelDataType) -> VoxelDataType
+    func valueAt(_ combined: SIMD3<VoxelDataType>) -> VoxelDataType
 }
 
 /// Signed distance function
-struct SDF<T: SIMDScalar> {
-    let _function: (T, T, T) -> T
+public struct SDF<T: SIMDScalar> {
+    public let _function: (T, T, T) -> T
 
     init(_function: @escaping (T, T, T) -> T) {
         self._function = _function
     }
 
     @inlinable
-    func valueAt(x: T, y: T, z: T) -> T {
+    public func valueAt(x: T, y: T, z: T) -> T {
         _function(x, y, z)
     }
 
     @inlinable
-    func valueAt(_ combined: SIMD3<T>) -> T {
+    public func valueAt(_ combined: SIMD3<T>) -> T {
         _function(combined.x, combined.y, combined.z)
     }
 }
