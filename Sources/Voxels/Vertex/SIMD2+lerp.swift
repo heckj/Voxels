215c30eb6faba903d69f2cb2d5de1f8b8e961c43
Joe Heck <heckj@mac.com>
2024-07-30 13:26:59 -0700

    pushing down req versions, adding more SIMD acceleration

--- Sources/Voxels/Vertex/SIMD2+lerp.swift
+++ Sources/Voxels/Vertex/SIMD2+lerp.swift
@@ -1,5 +1,13 @@
+#if canImport(simd)
+    import simd
+#endif
+
 public extension SIMD2<Float> {
     func lerp(_ a: SIMD2<Float>, _ t: Float) -> SIMD2<Float> {
-        self + (a - self) * t
+        #if canImport(simd)
+            return simd.mix(self, a, t: t)
+        #else
+            self + (a - self) * t
+        #endif
     }
 }
