d88743a950c72d0698f7b5b111d8f0e0001dabdb
Joseph Heck <heckj@mac.com>
2024-08-14 12:43:40 -0700

    meshbuffer and basic quads (#20)

---
+++ Tests/VoxelsTests/MeshBufferTests.swift
@@ -0,0 +1,34 @@
+import Voxels
+import XCTest
+
+class MeshBufferTests: XCTestCase {
+    func testMeshBufferInitializer() throws {
+        let buffer = MeshBuffer()
+        XCTAssertTrue(buffer.indices.isEmpty)
+        XCTAssertTrue(buffer.normals.isEmpty)
+        XCTAssertTrue(buffer.positions.isEmpty)
+    }
+
+    func testMeshBufferAddQuad() throws {
+        var buffer = MeshBuffer()
+        buffer.addQuad(p1: SIMD3<Float>(0, 1, 0), p2: SIMD3<Float>(0, 0, 0), p3: SIMD3<Float>(1, 1, 0), p4: SIMD3<Float>(1, 0, 0))
+        XCTAssertEqual(buffer.indices.count, 6)
+        XCTAssertEqual(buffer.positions.count, 4)
+        XCTAssertEqual(buffer.normals.count, 4)
+
+        for i in buffer.indices {
+            XCTAssertTrue(i < buffer.positions.count)
+            XCTAssertTrue(i < buffer.normals.count)
+        }
+    }
+
+    func testMeshBufferClear() throws {
+        var buffer = MeshBuffer()
+        buffer.addQuad(p1: SIMD3<Float>(0, 1, 0), p2: SIMD3<Float>(0, 0, 0), p3: SIMD3<Float>(1, 1, 0), p4: SIMD3<Float>(1, 0, 0))
+        buffer.reset()
+
+        XCTAssertTrue(buffer.indices.isEmpty)
+        XCTAssertTrue(buffer.normals.isEmpty)
+        XCTAssertTrue(buffer.positions.isEmpty)
+    }
+}
