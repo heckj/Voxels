18e82877663b7d0ec53b7860395c56ab84e4e4f0
Joe Heck <heckj@mac.com>
2024-08-23 13:06:55 -0700

    break up surface net buffer to use meshbuffer, prep for integratation into a general renderer

--- Sources/Voxels/Renderers/Buffers+meshDescriptor.swift
+++ Sources/Voxels/Renderers/Buffers+meshDescriptor.swift
@@ -4,9 +4,9 @@
     public extension SurfaceNetsBuffer {
         func meshDescriptor() -> MeshDescriptor {
             var meshDescriptor = MeshDescriptor()
-            meshDescriptor.primitives = .triangles(indices)
-            meshDescriptor.positions = MeshBuffers.Positions(positions)
-            meshDescriptor.normals = MeshBuffers.Normals(normals)
+            meshDescriptor.primitives = .triangles(meshbuffer.indices)
+            meshDescriptor.positions = MeshBuffers.Positions(meshbuffer.positions)
+            meshDescriptor.normals = MeshBuffers.Normals(meshbuffer.normals)
             return meshDescriptor
         }
     }
