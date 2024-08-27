298068898c8d098dab47f1bf86ebcadd61b761a0
Joe Heck <heckj@mac.com>
2024-08-27 13:33:23 -0700

    initialization verification

--- Tests/VoxelsTests/VoxelIndexTests.swift
+++ Tests/VoxelsTests/VoxelIndexTests.swift
@@ -2,6 +2,18 @@ import Voxels
 import XCTest
 
 class VoxelIndexTests: XCTestCase {
+    func testIndexInit() throws {
+        XCTAssertNotNil(VoxelIndex(0, 0, 0))
+        XCTAssertNotNil(VoxelIndex(x: 0, y: 0, z: 0))
+
+        XCTAssertNotNil(VoxelIndex(0, 0, 0))
+        XCTAssertNotNil(VoxelIndex([0, 0, 0]))
+        let xyz = [1, 1, 1]
+        XCTAssertNotNil(VoxelIndex(xyz))
+        let ijk = (2, 2, 2)
+        XCTAssertNotNil(VoxelIndex(ijk))
+    }
+
     func testIndexComparable() throws {
         XCTAssertTrue(VoxelIndex(0, 0, 0) < VoxelIndex(2, 2, 2))
         XCTAssertFalse(VoxelIndex(0, 0, 0) > VoxelIndex(2, 2, 2))
