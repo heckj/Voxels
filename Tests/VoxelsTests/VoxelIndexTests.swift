73963fdd50e05779c7f15fa02fb5247bece58719
Joseph Heck <heckj@mac.com>
2024-08-28 14:25:01 -0700

    resetting neighbors to return a set of indices (#48)
    * resetting neighbors to return a set of indices
    
    * code cleanup

--- Tests/VoxelsTests/VoxelIndexTests.swift
+++ Tests/VoxelsTests/VoxelIndexTests.swift
@@ -23,4 +23,47 @@ class VoxelIndexTests: XCTestCase {
         XCTAssertTrue(VoxelIndex(0, 0, 0) != VoxelIndex(2, 2, 2))
         XCTAssertTrue(VoxelIndex(2, 2, 2) == VoxelIndex(2, 2, 2))
     }
+
+    // MARK: Neighbors
+
+    func testDistance() throws {
+        XCTAssertEqual(VoxelIndex.manhattan_distance(from: VoxelIndex(0, 0, 0), to: VoxelIndex(0, 0, 1)), 1)
+
+        XCTAssertEqual(VoxelIndex.manhattan_distance(from: VoxelIndex(0, 0, 0), to: VoxelIndex(0, 1, 1)), 2)
+
+        XCTAssertEqual(VoxelIndex.manhattan_distance(from: VoxelIndex(0, 0, 0), to: VoxelIndex(1, 1, 1)), 3)
+    }
+
+    func testNeighborsOfArray() throws {
+        let fiveByFive = VoxelArray<Int>(edge: 7, value: 1)
+
+        let distanceZeroNeighbors = try VoxelIndex.neighbors(distance: 0, origin: VoxelIndex(3, 2, 1), voxels: fiveByFive)
+        XCTAssertEqual(distanceZeroNeighbors.count, 1)
+
+        // verify index in neighbor is index position from original voxel storage
+        XCTAssertEqual(distanceZeroNeighbors.sorted().first, VoxelIndex(3, 2, 1))
+
+        let distanceOneNeighbors = try VoxelIndex.neighbors(distance: 1, origin: VoxelIndex(1, 1, 1), voxels: fiveByFive)
+        XCTAssertEqual(distanceOneNeighbors.count, 7)
+
+        let distanceTwoNeighbors = try VoxelIndex.neighbors(distance: 2, origin: VoxelIndex(2, 2, 2), voxels: fiveByFive)
+        XCTAssertEqual(distanceTwoNeighbors.count, 25)
+    }
+
+    func testNeighborsOfHash() throws {
+        var fiveByFive = VoxelHash<Int>()
+
+        // create cube in the middle
+        for i in 1 ... 3 {
+            for j in 1 ... 3 {
+                for k in 1 ... 3 {
+                    fiveByFive.set(VoxelIndex(i, j, k), newValue: 1)
+                }
+            }
+        }
+        XCTAssertEqual(fiveByFive.count, 27)
+
+        let neighbors = try VoxelIndex.neighbors(distance: 0, origin: VoxelIndex(2, 2, 2), voxels: fiveByFive, strategy: .raw)
+        XCTAssertEqual(neighbors.count, 1)
+    }
 }
