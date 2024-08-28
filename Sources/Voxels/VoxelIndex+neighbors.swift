6eca4fce22ddf6caa9f1cdfd4b8e1ccc44699ee8
Joe Heck <heckj@mac.com>
2024-08-28 13:28:11 -0700

    resetting neighbors to return a set of indices

---
+++ Sources/Voxels/VoxelIndex+neighbors.swift
@@ -0,0 +1,40 @@
+extension VoxelIndex {
+    
+    public static func manhattan_distance(from: VoxelIndex, to: VoxelIndex) -> Int {
+        abs(from.x - to.x) + abs(from.y - to.y) + abs(from.z - to.z)
+    }
+
+    public enum NeighborStrategy {
+        case raw
+        case opaque
+        case surface
+    }
+
+    public static func neighbors(distance: Int, origin: VoxelIndex, voxels: any VoxelAccessible, strategy: NeighborStrategy = .raw) throws -> Set<VoxelIndex> {
+        precondition(distance >= 0)
+        var indices: Set<VoxelIndex> = [origin]
+        
+        for i in origin.x - distance ... origin.x + distance {
+            for j in origin.y - distance ... origin.y + distance {
+                for k in origin.z - distance ... origin.z + distance {
+                    let computedIndex = VoxelIndex(i, j, k)
+                    if manhattan_distance(from: origin, to: computedIndex) <= distance {
+                        switch strategy {
+                        case .raw:
+                            indices.insert(computedIndex)
+                        case .opaque:
+                            if let voxelData = try voxels.value(computedIndex), voxelData.isOpaque() {
+                                indices.insert(computedIndex)
+                            }
+                        case .surface:
+                            if try voxels.isSurface(computedIndex) {
+                                indices.insert(computedIndex)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return indices
+    }
+}
