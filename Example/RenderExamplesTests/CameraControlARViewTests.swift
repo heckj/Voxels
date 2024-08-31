93ea11bd1e5691204d7ddc88fa571fb751e8f781
Joe Heck <heckj@mac.com>
2024-08-31 15:10:31 -0700

    translate, then rotate

--- Example/RenderExamplesTests/CameraControlARViewTests.swift
+++ Example/RenderExamplesTests/CameraControlARViewTests.swift
@@ -41,4 +41,52 @@ final class CameraControlARViewTests: XCTestCase {
         XCTAssertEqual(heading.y, 0, accuracy: 0.01)
         XCTAssertEqual(heading.z, 0, accuracy: 0.01)
     }
+
+    func testTranslationViaMatrixMult() throws {
+        let initialTranslation = Transform(scale: .one,
+                                           rotation: simd_quatf(),
+                                           translation: SIMD3<Float>(10, 5, 10))
+        let rotate_to_move: Transform = .init(
+            pitch: 0.0,
+            yaw: 0.0,
+            roll: 0
+        )
+        let translationAfterRotationTransform = Transform(scale: .one,
+                                                          rotation: simd_quatf(),
+                                                          translation: SIMD3<Float>(0, 0, 5))
+
+        // ORDER of operations is critical here to getting the correct transform:
+        // - identity -> rotation -> translation
+
+        // and NOTABLY - you can't multiply two translation matrices together to get
+        // them to add in sequence - it's addition for that!
+        let computed_transform = initialTranslation.matrix + rotate_to_move.matrix * translationAfterRotationTransform.matrix
+        let computedAsTranform = Transform(matrix: computed_transform)
+        XCTAssertEqual(computedAsTranform.translation, SIMD3<Float>(10, 5, 15))
+    }
+
+    func testTranslationViaMatrixMultRotated() throws {
+        let initialTranslation = Transform(scale: .one,
+                                           rotation: simd_quatf(),
+                                           translation: SIMD3<Float>(10, 5, 10))
+        let rotate_to_move: Transform = .init(
+            pitch: -Float.pi / 2.0,
+            yaw: 0.0,
+            roll: 0
+        )
+        let translationAfterRotationTransform = Transform(scale: .one,
+                                                          rotation: simd_quatf(),
+                                                          translation: SIMD3<Float>(0, 0, 5))
+
+        // ORDER of operations is critical here to getting the correct transform:
+        // - identity -> rotation -> translation
+
+        // and NOTABLY - you can't multiply two translation matrices together to get
+        // them to add in sequence - it's addition for that!
+        let computed_transform = initialTranslation.matrix + rotate_to_move.matrix * translationAfterRotationTransform.matrix
+        let computedAsTranform = Transform(matrix: computed_transform)
+        XCTAssertEqual(computedAsTranform.translation.x, 10, accuracy: 0.01)
+        XCTAssertEqual(computedAsTranform.translation.y, 10, accuracy: 0.01)
+        XCTAssertEqual(computedAsTranform.translation.z, 10, accuracy: 0.01)
+    }
 }
