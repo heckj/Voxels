d2c4bf10600c038601b644f7846a6760bbd2b8ff
Joe Heck <heckj@mac.com>
2024-09-01 14:10:54 -0700

    debug view work

--- Example/RenderExamples/RenderExamplesApp.swift
+++ Example/RenderExamples/RenderExamplesApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct RenderExamplesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // ContentView()
+            QuickCheckView()
         }
     }
 }
