8958c3332709d118d7e3eaa0678f126607482f5f
Joe Heck <heckj@mac.com>
2024-09-04 11:08:43 -0700

    bounds mandatory

--- Sources/Voxels/Renderers/FastBlockMesh.swift
+++ Sources/Voxels/Renderers/FastBlockMesh.swift
@@ -1,6 +1,6 @@
 public extension VoxelMeshRenderer {
     /// A fast and simple meshing algorithm that produces a single quad for every visible face of a block.
-    static func fastBlockMeshSurfaceFaces(_ v: some VoxelAccessible, scale: VoxelScale<Float>, within bounds: VoxelBounds? = nil) -> MeshBuffer {
+    static func fastBlockMeshSurfaceFaces(_ v: some VoxelAccessible, scale: VoxelScale<Float>, within bounds: VoxelBounds) -> MeshBuffer {
         let boundsForIndices: VoxelBounds = bounds ?? v.bounds
         var buffer = MeshBuffer()
 
