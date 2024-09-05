3582e8e0a00d1e1265b9664d8408e190df5fc6c5
Joe Heck <heckj@mac.com>
2024-09-05 15:38:58 -0700

    breaking off RealityKit into it's own view to keep from re-loading it (breaking dependencies), and explicitly re-rendering on voxel data updates through the bindings

--- Example/RenderExamples/ObservableVoxelData.swift
+++ Example/RenderExamples/ObservableVoxelData.swift
@@ -1,3 +1,4 @@
+import RealityKit
 import SwiftUI
 import Voxels
 
@@ -5,9 +6,29 @@ import Voxels
 @MainActor
 class ObservableVoxelData {
     public var wrappedVoxelData: VoxelHash<Float>
+    let renderer: SurfaceNetRenderer
+    let voxelEntity: ModelEntity
+    #if os(macOS)
+        let baseMaterial = SimpleMaterial(color: .lightGray, isMetallic: false)
+    #else
+        let baseMaterial = SimpleMaterial(color: .lightGray, isMetallic: false)
+    #endif
+
+    let clock = ContinuousClock()
 
     init(_ data: VoxelHash<Float>) {
+        renderer = SurfaceNetRenderer()
         wrappedVoxelData = data
+        let generatedMeshBuffer = try! renderer.render(voxelData: data,
+                                                       scale: .init(),
+                                                       within: data.bounds.expand(2))
+
+        guard let descriptor = generatedMeshBuffer.meshDescriptor() else {
+            fatalError("Invalid mesh - no descriptor")
+        }
+        let mesh = try! MeshResource.generate(from: [descriptor])
+        voxelEntity = ModelEntity(mesh: mesh, materials: [baseMaterial])
+        voxelEntity.name = "VoxelData"
     }
 
     func binding(_ index: VoxelIndex) -> Binding<String> {
@@ -20,12 +41,68 @@ class ObservableVoxelData {
         } set: { newStringValue in
             if newStringValue.isEmpty {
                 self.wrappedVoxelData[index] = nil
+                self.redoRender()
             } else {
                 if let newFloatValue = Float(newStringValue) {
                     self.wrappedVoxelData[index] = newFloatValue
+                    self.redoRender()
                 }
             }
         }
         return voxelSDFValueBinding
     }
+
+    func redoRender() {
+        let startTime = clock.now
+        let generatedMeshBuffer = try! renderer.render(voxelData: wrappedVoxelData,
+                                                       scale: .init(),
+                                                       within: wrappedVoxelData.bounds.expand(2))
+        let timeToRender = clock.now - startTime
+
+        print("Render Duration: \(timeToRender.formatted(.units(allowed: [.milliseconds, .microseconds], width: .abbreviated))) (\(timeToRender.description))")
+        guard let descriptor = generatedMeshBuffer.meshDescriptor() else {
+            return
+        }
+        let mesh = try! MeshResource.generate(from: [descriptor])
+        voxelEntity.model?.mesh = mesh
+    }
+
+//    func renderEntity(_ voxels: some VoxelAccessible) -> ModelEntity {
+//        // Get the Metal Device, then the library from the device
+//        guard let device = MTLCreateSystemDefaultDevice(),
+//              let library = device.makeDefaultLibrary()
+//        else {
+//            fatalError("Error creating default metal device.")
+//        }
+//
+//        let geometryModifier = CustomMaterial.GeometryModifier(named: "wireframeMaterialGeometryModifier", in: library)
+//
+//         Load a surface shader function named mySurfaceShader.
+//        let surfaceShader = CustomMaterial.SurfaceShader(named: "wireframeMaterialSurfaceShader", in: library)
+//        do {
+//        } catch {
+//            fatalError(error.localizedDescription)
+//        }
+//
+//            let material = try CustomMaterial(surfaceShader: surfaceShader, geometryModifier: geometryModifier, lightingModel: .clearcoat)
+//            let material = try CustomMaterial(from: baseMaterial, surfaceShader: surfaceShader)
+//            let material = try CustomMaterial(surfaceShader: surfaceShader, lightingModel: .unlit)
+//
+    // #if os(macOS)
+//    let baseMaterial = SimpleMaterial(color: .lightGray, isMetallic: false)
+    // #else
+//    let baseMaterial = SimpleMaterial(color: .lightGray, isMetallic: false)
+    // #endif
+//        let generatedMeshBuffer = try! renderer.render(voxelData: voxels,
+//                                                       scale: .init(),
+//                                                       within: voxels.bounds.expand(2))
+//
+//        guard let descriptor = generatedMeshBuffer.meshDescriptor() else {
+//            fatalError("Invalid mesh - no descriptor")
+//        }
+//        let mesh = try! MeshResource.generate(from: [descriptor])
+//        let entity = ModelEntity(mesh: mesh, materials: [baseMaterial])
+//        entity.name = "VoxelData"
+//        return entity
+//    }
 }
