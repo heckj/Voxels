3b25ee2e01aca4a9f376652d9f41625f1142d077
Joe Heck <heckj@mac.com>
2024-09-08 10:54:02 -0700

    fixing dump output to layer by 'Y' (height) with an x-z crosssection

--- Sources/Voxels/VoxelStorage/VoxelHash+dump.swift
+++ Sources/Voxels/VoxelStorage/VoxelHash+dump.swift
@@ -2,10 +2,10 @@ import Foundation
 
 extension VoxelHash where T == Float {
     func dump() {
-        for z in (bounds.min.z ... bounds.max.z).reversed() {
-            print("Frame (Z): \(z)")
-            for y in bounds.min.y ... bounds.max.y {
-                var line: String = ""
+        for y in (bounds.min.y ... bounds.max.y).reversed() {
+            print("Frame (Y): \(y)")
+            for z in bounds.min.z ... bounds.max.z {
+                var line = ""
                 for x in bounds.min.x ... bounds.max.x {
                     let index = VoxelIndex(x, y, z)
                     if let value = self[index] {
