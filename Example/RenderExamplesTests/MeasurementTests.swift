2f9ae23af8005307c05e55abdbc98e290aac2ae1
Joe Heck <heckj@mac.com>
2024-09-10 11:33:25 -0700

    doing some baseline measurements just to check the ballpark distances

---
+++ Example/RenderExamplesTests/MeasurementTests.swift
@@ -0,0 +1,63 @@
+import Heightmap
+import RealityKit
+import RenderExamples
+import Voxels
+import XCTest
+
+final class GeneralMeasurementTests: XCTestCase {
+    func testGenerateHeightmap() throws {
+        // ~ 0.361 seconds
+        measure {
+            let _ = Heightmap(width: 500, height: 500, seed: 437_347_632)
+        }
+    }
+
+    func testGenerateVoxelHeightmap() throws {
+        let heightmap = Heightmap(width: 500, height: 500, seed: 437_347_632)
+
+        // ~ 12.2 seconds
+        measure {
+            let _ = VoxelHash<Float>.heightmap(heightmap.contents,
+                                               width: heightmap.width,
+                                               maxVoxelHeight: 200)
+        }
+    }
+
+    func testSurfaceNetRenderVoxelizedHeightmap() throws {
+        let heightmap = Heightmap(width: 100, height: 100, seed: 437_347_632)
+        let voxels = VoxelHash<Float>.heightmap(heightmap.contents,
+                                                width: heightmap.width,
+                                                maxVoxelHeight: 200)
+
+        // ~ 11.8 seconds
+        measure {
+            let _ = try! SurfaceNetRenderer().render(voxelData: voxels,
+                                                     scale: .init(),
+                                                     within: voxels.bounds)
+        }
+    }
+
+    func testMarchingCubesRenderVoxelizedHeightmap() throws {
+        let heightmap = Heightmap(width: 100, height: 100, seed: 437_347_632)
+        let voxels = VoxelHash<Float>.heightmap(heightmap.contents,
+                                                width: heightmap.width,
+                                                maxVoxelHeight: 200)
+
+        // ~ 8.47 seconds
+        measure {
+            let _ = MarchingCubesRenderer().marching_cubes(data: voxels, scale: .init())
+        }
+    }
+
+    func testBlockMeshRenderVoxelizedHeightmap() throws {
+        let heightmap = Heightmap(width: 100, height: 100, seed: 437_347_632)
+        let voxels = VoxelHash<Float>.heightmap(heightmap.contents,
+                                                width: heightmap.width,
+                                                maxVoxelHeight: 200)
+
+        // ~ 1.87 seconds
+        measure {
+            let _ = VoxelMeshRenderer.fastBlockMeshSurfaceFaces(voxels, scale: .init(), within: voxels.bounds)
+        }
+    }
+}
