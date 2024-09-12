b28459f916c4aff6a5752b545d03f1bb210f0d61
Joe Heck <heckj@mac.com>
2024-09-12 15:42:43 -0700

    moving all the static functions for converting height maps to HeightmapConverter

--- Tests/VoxelsTests/HeightmapConversionTests.swift
+++ Tests/VoxelsTests/HeightmapConversionTests.swift
@@ -8,14 +8,14 @@ final class HeightmapConversionTests: XCTestCase {
         // we need to know the 'unit height' equivalent of the centroid of a voxel.
 
         // for a maxHeight of 6 (Voxels 0...5)
-        XCTAssertEqual(VoxelHash<Float>.unitCentroidValue(0, maxHeight: 6), 0.0, accuracy: 0.01)
-        XCTAssertEqual(VoxelHash<Float>.unitCentroidValue(1, maxHeight: 6), 0.2, accuracy: 0.01)
-        XCTAssertEqual(VoxelHash<Float>.unitCentroidValue(2, maxHeight: 6), 0.4, accuracy: 0.01)
-        XCTAssertEqual(VoxelHash<Float>.unitCentroidValue(3, maxHeight: 6), 0.6, accuracy: 0.01)
-        XCTAssertEqual(VoxelHash<Float>.unitCentroidValue(4, maxHeight: 6), 0.8, accuracy: 0.01)
-        XCTAssertEqual(VoxelHash<Float>.unitCentroidValue(5, maxHeight: 6), 1.0, accuracy: 0.01)
+        XCTAssertEqual(HeightmapConverter.unitCentroidValue(0, maxHeight: 6), 0.0, accuracy: 0.01)
+        XCTAssertEqual(HeightmapConverter.unitCentroidValue(1, maxHeight: 6), 0.2, accuracy: 0.01)
+        XCTAssertEqual(HeightmapConverter.unitCentroidValue(2, maxHeight: 6), 0.4, accuracy: 0.01)
+        XCTAssertEqual(HeightmapConverter.unitCentroidValue(3, maxHeight: 6), 0.6, accuracy: 0.01)
+        XCTAssertEqual(HeightmapConverter.unitCentroidValue(4, maxHeight: 6), 0.8, accuracy: 0.01)
+        XCTAssertEqual(HeightmapConverter.unitCentroidValue(5, maxHeight: 6), 1.0, accuracy: 0.01)
         // stepping above the max height should result in > 1.0 values
-        XCTAssertEqual(VoxelHash<Float>.unitCentroidValue(6, maxHeight: 6), 1.2, accuracy: 0.01)
+        XCTAssertEqual(HeightmapConverter.unitCentroidValue(6, maxHeight: 6), 1.2, accuracy: 0.01)
     }
 
     func testAccessingFloors() throws {
@@ -33,34 +33,34 @@ final class HeightmapConversionTests: XCTestCase {
         XCTAssertEqual(unitFloatValues[2][3], 0.6)
         XCTAssertEqual(heightmap[3, 2], 0.6)
 
-        XCTAssertEqual(VoxelHash<Float>.unitSurfaceIndexValue(XZIndex(x: 0, z: 0), heightmap: heightmap, maxHeight: 10), 0)
-        XCTAssertEqual(VoxelHash<Float>.unitSurfaceIndexValue(XZIndex(x: 1, z: 0), heightmap: heightmap, maxHeight: 10), 1)
-        XCTAssertEqual(VoxelHash<Float>.unitSurfaceIndexValue(XZIndex(x: 2, z: 0), heightmap: heightmap, maxHeight: 10), 2)
-        XCTAssertEqual(VoxelHash<Float>.unitSurfaceIndexValue(XZIndex(x: 3, z: 0), heightmap: heightmap, maxHeight: 10), 3)
+        XCTAssertEqual(HeightmapConverter.unitSurfaceIndexValue(XZIndex(x: 0, z: 0), heightmap: heightmap, maxHeight: 10), 0)
+        XCTAssertEqual(HeightmapConverter.unitSurfaceIndexValue(XZIndex(x: 1, z: 0), heightmap: heightmap, maxHeight: 10), 1)
+        XCTAssertEqual(HeightmapConverter.unitSurfaceIndexValue(XZIndex(x: 2, z: 0), heightmap: heightmap, maxHeight: 10), 2)
+        XCTAssertEqual(HeightmapConverter.unitSurfaceIndexValue(XZIndex(x: 3, z: 0), heightmap: heightmap, maxHeight: 10), 3)
         // highest value in the map
-        XCTAssertEqual(VoxelHash<Float>.unitSurfaceIndexValue(XZIndex(x: 3, z: 2), heightmap: heightmap, maxHeight: 10), 5)
+        XCTAssertEqual(HeightmapConverter.unitSurfaceIndexValue(XZIndex(x: 3, z: 2), heightmap: heightmap, maxHeight: 10), 5)
     }
 
     func testTwoDIndexNeighborsFrom() throws {
-        let neighborsCorner = VoxelHash<Float>.twoDIndexNeighborsFrom(position: XZIndex(x: 0, z: 0), widthCount: 3, depthCount: 3)
+        let neighborsCorner = HeightmapConverter.twoDIndexNeighborsFrom(position: XZIndex(x: 0, z: 0), widthCount: 3, depthCount: 3)
         XCTAssertEqual(neighborsCorner.count, 3)
 
-        let neighborsMid = VoxelHash<Float>.twoDIndexNeighborsFrom(position: XZIndex(x: 1, z: 1), widthCount: 3, depthCount: 3)
+        let neighborsMid = HeightmapConverter.twoDIndexNeighborsFrom(position: XZIndex(x: 1, z: 1), widthCount: 3, depthCount: 3)
         XCTAssertEqual(neighborsMid.count, 8)
 
-        let neighborsSide = VoxelHash<Float>.twoDIndexNeighborsFrom(position: XZIndex(x: 1, z: 0), widthCount: 3, depthCount: 3)
+        let neighborsSide = HeightmapConverter.twoDIndexNeighborsFrom(position: XZIndex(x: 1, z: 0), widthCount: 3, depthCount: 3)
         XCTAssertEqual(neighborsSide.count, 5)
 
-        let neighborsFarCorner = VoxelHash<Float>.twoDIndexNeighborsFrom(position: XZIndex(x: 3, z: 2), widthCount: 4, depthCount: 3)
+        let neighborsFarCorner = HeightmapConverter.twoDIndexNeighborsFrom(position: XZIndex(x: 3, z: 2), widthCount: 4, depthCount: 3)
         XCTAssertEqual(neighborsFarCorner.count, 3)
     }
 
     func testPointDistanceToLine() throws {
         let p = SIMD3<Float>(0, 0, 0)
-        let distance = VoxelHash<Float>.distanceFromPointToLine(p: p, x1: SIMD3<Float>(1, 1, 0), x2: SIMD3<Float>(1, -1, 0))
+        let distance = HeightmapConverter.distanceFromPointToLine(p: p, x1: SIMD3<Float>(1, 1, 0), x2: SIMD3<Float>(1, -1, 0))
         XCTAssertEqual(distance, 1)
 
-        XCTAssertEqual(VoxelHash<Float>.distanceFromPointToLine(p: p, x1: SIMD3<Float>(2, 0, 0), x2: SIMD3<Float>(0, 2, 0)), sqrt(2.0), accuracy: 0.01)
+        XCTAssertEqual(HeightmapConverter.distanceFromPointToLine(p: p, x1: SIMD3<Float>(2, 0, 0), x2: SIMD3<Float>(0, 2, 0)), sqrt(2.0), accuracy: 0.01)
     }
 
     func testHeightMapToVoxel() throws {
@@ -71,7 +71,7 @@ final class HeightmapConversionTests: XCTestCase {
             [0.4, 0.3, 0.4, 0.2],
         ]
         let heightmap = Heightmap(Array(unitFloatValues.joined()), width: 4)
-        let voxels = VoxelHash<Float>.heightmap(heightmap, maxVoxelHeight: 10)
+        let voxels = HeightmapConverter.heightmap(heightmap, maxVoxelHeight: 10)
         XCTAssertEqual(voxels.count, 93)
         for voxelValue in voxels {
             XCTAssertTrue(!voxelValue.isNaN)
@@ -91,14 +91,14 @@ final class HeightmapConversionTests: XCTestCase {
             [0.1, 0.5],
         ]
         let heightmap = Heightmap(Array(unitFloatValues.joined()), width: 2)
-        let voxels = VoxelHash<Float>.heightmap(heightmap, maxVoxelHeight: 5)
+        let voxels = HeightmapConverter.heightmap(heightmap, maxVoxelHeight: 5)
         XCTAssertEqual(voxels.count, 16)
         XCTAssertEqual(voxels.bounds.max.y, 5)
         for voxelValue in voxels {
             XCTAssertTrue(!voxelValue.isNaN)
         }
 
-        let dumpedHeights = voxels.heightmap()
+        let dumpedHeights: Heightmap = HeightmapConverter.heightmap(from: voxels)
         XCTAssertEqual([0.1, 0.1, 0.1, 0.5], dumpedHeights.contents)
     }
 
@@ -111,7 +111,7 @@ final class HeightmapConversionTests: XCTestCase {
             [0.4, 0.4, 0.4, 0.4, 0.0],
         ]
         let heightmap = Heightmap(Array(unitFloatValues.joined()), width: 5)
-        let voxels = VoxelHash<Float>.heightmap(heightmap, maxVoxelHeight: 5)
+        let voxels = HeightmapConverter.heightmap(heightmap, maxVoxelHeight: 5)
         XCTAssertEqual(voxels.count, 99)
         for voxelValue in voxels {
             XCTAssertTrue(!voxelValue.isNaN)
