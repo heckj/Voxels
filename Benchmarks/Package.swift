7084967e083c38c14b3420e67b90ed3213bffc89
Joe Heck <heckj@mac.com>
2024-09-14 16:21:05 -0700

    setting up single benchmarks

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -15,8 +15,19 @@ let package = Package(
         .package(path: "../"),
         .package(url: "https://github.com/tayloraswift/swift-noise", from: "1.1.0"),
         .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.1"),
+        .package(url: "https://github.com/ordo-one/package-benchmark", .upToNextMajor(from: "1.4.0")),
     ],
     targets: [
+        .executableTarget(
+            name: "VoxelBenchmark",
+            dependencies: [
+                .product(name: "Benchmark", package: "package-benchmark"),
+            ],
+            path: "Benchmarks/VoxelBenchmark",
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+            ]
+        ),
         .executableTarget(
             name: "voxel-benchmarks",
             dependencies: [
