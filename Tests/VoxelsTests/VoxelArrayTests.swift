cb63cc861560d8f63aef0a79a3e83c48d2e5b5e1
Joe Heck <heckj@mac.com>
2024-10-18 16:17:54 -0700

    add VoxelArray initializer using bounds, fix subscript set with a VoxelIndex, and test the results

--- Tests/VoxelsTests/VoxelArrayTests.swift
+++ Tests/VoxelsTests/VoxelArrayTests.swift
@@ -19,6 +19,43 @@ class VoxelArrayTests: XCTestCase {
         XCTAssertEqual(v.bounds.size, 27)
     }
 
+    func testVoxelArrayBoundsInitializer() throws {
+        let bounds = VoxelBounds(min: (0, 0, 0), max: (9, 4, 9))
+        XCTAssertEqual(bounds.size, 10 * 5 * 10)
+
+        let v = VoxelArray<Int>(bounds: bounds, initialValue: 1)
+
+        // internal access
+        XCTAssertEqual(v._contents.count, 10 * 5 * 10)
+        for internalValue in v._contents {
+            XCTAssertEqual(internalValue, 1)
+        }
+
+        // external access
+        XCTAssertEqual(v.value(VoxelIndex(x: 0, y: 0, z: 0)), 1)
+        XCTAssertEqual(v.value(VoxelIndex(x: 1, y: 1, z: 1)), 1)
+        XCTAssertEqual(v.value(VoxelIndex(x: 2, y: 2, z: 2)), 1)
+
+        XCTAssertEqual(v.bounds.size, 10 * 5 * 10)
+        XCTAssertEqual(v.indices, v.bounds.indices)
+    }
+
+    func testUpdatingVoxelArray() throws {
+        let index = VoxelIndex(0, 1, 1)
+        var voxels = VoxelArray<Int>(edge: 3, value: 1)
+
+        voxels.set(index, newValue: 5)
+        XCTAssertEqual(voxels[index], 5)
+    }
+
+    func testUpdatingVoxelArraySubscript() throws {
+        let index = VoxelIndex(0, 1, 1)
+        var voxels = VoxelArray<Int>(edge: 3, value: 1)
+
+        voxels[index] = 5
+        XCTAssertEqual(voxels[index], 5)
+    }
+
     func testVoxelArraySequence() throws {
         var voxels = VoxelArray<Int>(edge: 3, value: 1)
         voxels.set(VoxelIndex(1, 1, 1), newValue: 2)
