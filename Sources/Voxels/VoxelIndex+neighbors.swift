4b74151a54634b806b0a1fc55de2645f592ed0aa
Joseph Heck <heckj@mac.com>
2024-11-16 16:58:51 -0800

    adding instance functions for manhatten neighbors 1 and 2 (#130)

--- Sources/Voxels/VoxelIndex+neighbors.swift
+++ Sources/Voxels/VoxelIndex+neighbors.swift
@@ -1,4 +1,41 @@
 public extension VoxelIndex {
+    /// Returns the manhattan neighbors with a distance of one from this index.
+    @inlinable
+    func manhattan_1_neighbors() -> [VoxelIndex] {
+        [
+            adding(VoxelIndex(1, 0, 0)),
+            adding(VoxelIndex(-1, 0, 0)),
+            adding(VoxelIndex(0, 1, 0)),
+            adding(VoxelIndex(0, -1, 0)),
+            adding(VoxelIndex(0, 0, 1)),
+            adding(VoxelIndex(0, 0, -1)),
+        ]
+    }
+
+    /// Returns the manhattan neighbors with a distance of two from this index.
+    @inlinable
+    func manhattan_2_neighbors() -> [VoxelIndex] {
+        var neighbors: [VoxelIndex] = []
+        for i in x - 1 ... x + 1 {
+            for j in y - 1 ... y + 1 {
+                for k in z - 1 ... z + 1 {
+                    let new = VoxelIndex(i, j, k)
+                    let distance = VoxelIndex.manhattan_distance(from: self, to: new)
+                    if distance == 1 || distance == 2 {
+                        neighbors.append(new)
+                    }
+                }
+            }
+        }
+        neighbors.append(adding(VoxelIndex(2, 0, 0)))
+        neighbors.append(adding(VoxelIndex(-2, 0, 0)))
+        neighbors.append(adding(VoxelIndex(0, 2, 0)))
+        neighbors.append(adding(VoxelIndex(0, -2, 0)))
+        neighbors.append(adding(VoxelIndex(0, 0, 2)))
+        neighbors.append(adding(VoxelIndex(0, 0, -2)))
+        return neighbors
+    }
+
     static func manhattan_distance(from: VoxelIndex, to: VoxelIndex) -> Int {
         abs(from.x - to.x) + abs(from.y - to.y) + abs(from.z - to.z)
     }
